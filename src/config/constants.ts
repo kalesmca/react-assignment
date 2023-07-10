2064f7a3126c728cffac1ef74c0486a41c15bd62
kaleeswaran <kalesmca@gmail.com>
2023-07-11 01:15:25 +0530

    alignment issue fixed

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,3 +3,4 @@ export const API_KEY = "live_AUhtHu2H3mMR4J2R3OJWWyzYdHKVuHOVFSrMTMJjn9HpSxrYUpI
 export const BASE_URL = "https://api.thedogapi.com"
 export const DOGS_LIST_URL = "/v1/breeds/"
 export const BREED_SEARCH_URL = "/v1/breeds/search"
+export const IMG_BASE_BATH = "https://cdn2.thedogapi.com/images/"
--- src/containers/dashboard/dashboardComponent.tsx
+++ src/containers/dashboard/dashboardComponent.tsx
@@ -4,8 +4,11 @@ import { getAllDogs, getDogsByBreedName } from "../../redux/actions/dashboard";
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { useDebounce } from "../../customHooks/useDebounce";
-
+import * as CONSTANTS from '../../config/constants';
+import './dashboard.css'
 const Dashboard = () =>{
     const dashboard:dashboardState = useSelector((state:any)=>state.dashboard);
     const dispatch = useDispatch();
@@ -17,7 +20,9 @@ const Dashboard = () =>{
 
     },[])
     useEffect(()=>{
-        dispatch(getDogsByBreedName(searchQuery))
+            dispatch(getDogsByBreedName(searchQuery))
+        
+        
     },[searchQuery])
     useEffect(()=>{
         console.log("state:", dashboard);
@@ -27,7 +32,7 @@ const Dashboard = () =>{
         <div>
             <Form>
             <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail" style={{ marginTop: "-30px" }}>
+            <Form.Group as={Col} controlId="formGridEmail">
 
                 {/* <Form.Label></Form.Label> */}
                 <Form.Control type="text" value={query} onChange={(e)=>{setQuery(e.target.value)}} placeholder="Seach by Breed"
@@ -35,6 +40,39 @@ const Dashboard = () =>{
                 </Form.Group>
                 </Row>
             </Form>
+            <div>
+                {
+                    dashboard?.dogList?.length ? dashboard.dogList.map((dog:any, dIndex:number) =>{
+                        return(
+                            (<div className="card-container"><Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={ CONSTANTS.IMG_BASE_BATH+dog.reference_image_id+'.jpg'} />
+                            <Card.Body>
+                              <Card.Title>{dog.name}</Card.Title>
+                              <Card.Text>
+                              <div className="card-body-content">
+                                    <span className="card-body-title">Origin</span>
+                                    <span className="card-body-value">{dog.origin}</span>
+                                </div>
+                                <div className="card-body-content">
+                                    <span className="card-body-title">life_span</span>
+                                    <span className="card-body-value">{dog.life_span}</span>
+                                </div>
+                                <div className="card-body-content">
+                                    <span className="card-body-title">Temperament</span>
+                                    <span className="card-body-value">{dog.temperament}</span>
+                                </div>
+
+                               
+                                
+                              </Card.Text>
+                                                            
+                            </Card.Body>
+                          </Card></div>) 
+                        )
+                    })
+                    : (<div>No Data Found.</div>)
+                }
+            </div>
         </div>
     )
 }
